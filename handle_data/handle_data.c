43a0b8a36d5a4498529ee6bb859e73e231daf023
AHEADer <dqawveird@gmail.com>
2015-11-04 20:58:33 +0800

    commit

--- handle_data/handle_data.c
+++ handle_data/handle_data.c
@@ -3,7 +3,20 @@
 
 int main(void)
 {
-    printf("hello world!\n");
+    //printf("hello world!\n");
+    FILE *config;
+    char config_file_path[]=CONFIG_FILE;
+    if(!(config = fopen(config_file_path,"r")))
+    {
+        printf("error while reading config file\n");
+        exit(-1);
+    }
+    if(!read_config(config))
+    {
+        printf("error while reading config\n");
+        exit(-1);
+    }
+    //fscanf(config,)
     return 0;
 }
 
@@ -16,6 +29,33 @@ int main(void)
 int read_config(FILE *config)
 {
     //sscanf
+    int line;
+    char StrLine[80];
+    while (!feof(config)) 
+    {
+        char local_mac_address[18];
+        char tmp_mac_address[18]; 
+        fgets(StrLine,80,config);  //read a line
+        //printf("%s\n", StrLine); //print it
+        if (strstr(StrLine,"local mac"))
+        {
+            sscanf(StrLine, "%*[^:]:%s" , tmp_mac_address ) ;
+            printf("local mac is %s\n",tmp_mac_address );
+        }
+        else
+            if (strstr(StrLine,"destination mac"))
+        {
+            sscanf(StrLine, "%*[^:]:%s" , tmp_mac_address ) ;
+            printf("destination mac is %s\n",tmp_mac_address );
+        }
+            else
+                {
+                    sscanf(StrLine, "%[a-zA-Z0-9]" , tmp_mac_address );
+                    printf("destination mac is %s\n",tmp_mac_address );
+                }
+
+    }
+    printf("read config over!\n"); 
     char* content;
     return 1;
 }
@@ -24,7 +64,7 @@ int read_config(FILE *config)
  *to store destination mac address to a 
  *created link
  */
-int store_dst_mac_address(char* mac_address)
+int store_mac_address(char* mac_address)
 {
     
     return 1;
@@ -33,6 +73,7 @@ int store_dst_mac_address(char* mac_address)
 /*get data and decide to add,delete or
  *sync
  */
+ /*
 int data_handle()
 {
     return 1;
@@ -51,7 +92,8 @@ int del_in_file()
 int sync_in_file()
 {
     return 1;
-}
+}*/
+
 /* TODO
 int free_link()
 {
--- handle_data/handle_data.h
+++ handle_data/handle_data.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CONFIG_FILE "Config.txt"
+
 typedef struct destination_mac{
     char* mac_address;
     struct destination_mac* next;
@@ -17,6 +19,11 @@ typedef struct student_info{
     struct student_info* next;
 }STUDENT_INFO;
 
+/*struct name:stu_full
+ *
+ *to provide category and content 
+ *information
+ */
 struct stu_full
 {
     enum S_TYPE
@@ -29,12 +36,23 @@ struct stu_full
     struct stu_full *next;
 };
 
+/*struct name:stu_id
+ *
+ *if the state is del,just use it
+ *to del students who has the same
+ *id
+ */
 struct stu_id
 {
     char *id;
     struct stu_id *next;
 };
 
+/*struct name:
+ *
+ *the data passing format
+ *
+ */
 struct slice{
     enum P_TYPE
     {
@@ -43,6 +61,8 @@ struct slice{
         SYN,
         ERROR
     } type;
+    
+    time_t timestamp;
     
     char smac[6];
     
@@ -67,5 +87,6 @@ typedef struct session
     struct slice* info;
 }SESSION;
 
+int read_config(FILE *config);
 
 #endif
