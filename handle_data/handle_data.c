781c7cf2157ff61493eaab3b476e47e909d2d09a
AHEADer <dqawveird@gmail.com>
2015-11-04 13:58:11 +0800

    change

---
+++ handle_data/handle_data.c
@@ -0,0 +1,61 @@
+#include "handle_data.h"
+
+
+int main(void)
+{
+    printf("hello world!\n");
+    return 0;
+}
+
+
+/*function name:read_config
+ *
+ *to read Config.txt and get the local mac
+ *and destination mac
+ */
+int read_config(FILE *config)
+{
+    //sscanf
+    char* content;
+    return 1;
+}
+
+/*function name:store_dst_mac_address
+ *to store destination mac address to a 
+ *created link
+ */
+int store_dst_mac_address(char* mac_address)
+{
+    
+    return 1;
+}
+
+/*get data and decide to add,delete or
+ *sync
+ */
+int data_handle()
+{
+    return 1;
+}
+
+int add_in_file()
+{
+    return 1;
+}
+
+int del_in_file()
+{
+    return 1;
+}
+
+int sync_in_file()
+{
+    return 1;
+}
+/* TODO
+int free_link()
+{
+    return 1;
+}
+*/
+
--- handle_data/handle_data.h
+++ handle_data/handle_data.h
@@ -1,2 +1,71 @@
-#include <stdio.h>
+#ifndef HANDLE_DATA_H
+#define HANDLE_DATA_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct destination_mac{
+    char* mac_address;
+    struct destination_mac* next;
+}DST_MAC;
+
+typedef struct student_info{
+    char* faculty;
+    char* id;
+    char* name;
+    struct student_info* next;
+}STUDENT_INFO;
+
+struct stu_full
+{
+    enum S_TYPE
+    {
+        ID,
+        NAME,
+        ACADEMY
+    } type;
+    char *content;
+    struct stu_full *next;
+};
+
+struct stu_id
+{
+    char *id;
+    struct stu_id *next;
+};
+
+struct slice{
+    enum P_TYPE
+    {
+        ADD,
+        DEL,
+        SYN,
+        ERROR
+    } type;
+    
+    char smac[6];
+    
+    struct
+    {
+        int start:1;
+        int end:1;
+        int sli_num:22;
+    };
+    int session;
+    union
+    {
+        struct stu_full* content;
+        struct stu_id *id_content;
+    };
+};
+
+typedef struct session
+{
+    int id;
+    struct session* next;
+    struct slice* info;
+}SESSION;
+
+
+#endif
