99ecb00840f78232fa92ff00459fe91b9b58c4f3
hyrathb <hyrathb@gmail.com>
2015-11-05 20:12:27 +0800

    Add utils

--- parser/parser.c
+++ parser/parser.c
@@ -1,67 +1,12 @@
 #include "packet_struct.h"
 #include "parser.h"
+#include "utils.h"
+
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-static const unsigned char rbits[8] = {0x0, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, 0x7f};
-static const unsigned char lbits[8] = {0x0, 0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe};
 static const char max_len[4] = {0, 12, 16, 64};
-
-void swap_byte(unsigned char *b1, unsigned char *b2)
-{
-    unsigned char temp = *b1;
-    *b1 = *b2;
-    *b2 = temp;
-}
-
-void ntoh_nbytes(unsigned char *dst, size_t n, size_t silly_bits)
-{
-    if (n <= 8)
-        return;
-    unsigned char *h=dst, *t=dst+n-1;
-    unsigned char silly=*h & lbits[silly];
-    while (h<t)
-    {
-        swap_byte(h, t);
-        ++h;
-        --t;
-    }
-    if (silly_bits)
-    {
-        char *h=dst, *t=dst+n-1;
-        *t <<= silly_bits;
-        unsigned char left = silly;
-        while (h <= t)
-        {
-            unsigned char tleft = (*h & rbits[silly_bits]) << (8 - silly_bits);
-            *h >>= silly_bits;
-            *h |= left;
-            left = tleft;
-            ++h;
-        }
-    }
-}
-
-void ntoh(unsigned char *dbuf, size_t len)
-{
-   ntoh_6bytes(dbuf);
-   dbuf += 6;
-   
-   ntoh_6bytes(dbuf);
-   dbuf += 6;
-   
-   ntoh_2bytes(dbuf);
-   dbuf += 2;
-   
-   dbuf += 1;
-   
-   ntoh_nbytes(dbuf, 3, 2);
-   dbuf += 3;
-   
-   ntoh_4bytes(dbuf);
-   dbuf += 4;
-}
 
 void fill_slice_common(struct slice *s, const struct packet *p)
 {
--- parser/parser.h
+++ parser/parser.h
@@ -25,18 +25,8 @@ struct packet
         int session;
     };
     char tlvs[];
-    
 };
-#define ntoh_2bytes(buf) ntoh_nbytes(buf, 2, 0)
-#define ntoh_4bytes(buf) ntoh_nbytes(buf, 4, 0)
-#define ntoh_6bytes(buf) ntoh_nbytes(buf, 6, 0)
-#define ntoh_8bytes(buf) ntoh_nbytes(buf, 8, 0)
 
-void swap_byte(unsigned char *b1, unsigned char *b2);
-
-void ntoh_nbytes(unsigned char *buf, size_t len, size_t silly_bits);
-
-void ntoh(unsigned char *dbuf, size_t len);
 
 void fill_slice_common(struct slice *s, const struct packet *p);
 
