f61dbd3f33a68e8be0f0490de4117f3a9d2f0300
kaixili <kaixili@hustunqiue.com>
2015-11-06 17:37:40 +0800

    left something to do

--- async/async.c
+++ async/async.c
@@ -1,66 +1,18 @@
 #include "async.h"
-#define BUFFER_MAX 1049
 
-struct packet
-{
-    char smac[6];
-    char dmac[6];
-    short pro_type;
-    struct
-    {
-        char c_type;
-        int start:1;
-        int end:1;
-        int slice:22;
-        int session;
-    };
-    char tlvs[];
+int _csismp_send(int send_socket, const char *buffer, int len);
 
-};
-
-int p_sync_callback();     // Send all data message
-int p_reply_callback();    // ACK or RJT
-int p_read_callback();     // Recive all message
-
-
-//组织整个以太网数据帧
-struct sockaddr_ll{
-    unsigned short sll_family;     /* 总是 AF_PACKET */
-    unsigned short sll_protocol;   /* 物理层的协议 */   //   0x1122
-    int            sll_ifindex;    /* 接口号 */         //  0表示处理所有接口
-    unsigned short sll_hatype;     /* 报头类型 */       //  ARPHRD_ETHER
-    unsigned char  sll_pkttype;    /* 分组类型 */      //   包含分组类型。
-    //目前，有效的分组类型有：
-    //目标地址是本地主机的分组用的 PACKET_HOST，
-    //物理层广播分组用的 PACKET_BROADCAST ，
-    //发送到一个物理层多路广播地址的分组用的 PACKET_MULTICAST，
-    //在混杂(promiscuous)模式下的设备驱动器发向其他主机的分组用的 PACKET_OTHERHOST，
-    //源于本地主机的分组被环回到分组套接口用的 PACKET_OUTGOING。这些类型只对接收到的分组有意义
-    unsigned char  sll_halen;      /* 地址长度 */      //指示物理层
-    unsigned char  sll_addr[8];    /* 物理层地址 */
-};
-
-
-struct ethhdr
-{
-    unsigned char h_dest[ETH_ALEN]; /* destination eth addr */
-    unsigned char h_source[ETH_ALEN]; /* source ether addr */
-    unsigned short h_proto; /* packet type ID field */
-};
-/*********************/
-
-int async_main(int argc, char **argv[])
+int main(int argc, char **argv[])
 {
     int listen_socket;
     listen_socket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    //报收本机收到的所有二层报文 PF_PACKET or PF_INET
-    //接收数据不包括数据链路帧头
+
     if ( listen_socket < 0 )
     {
         fprintf(stderr ,"Failed to create socket\n");
         exit(-1);
     }
-    //
+
     struct ifreq ifr;
     /*
     struct ifaddrs *ifaddr;
@@ -69,82 +21,92 @@ int async_main(int argc, char **argv[])
     if (ioctl(listen_socket, SIOCGIFINDEX, &ifr) == -1){
         fprintf(stderr, "Unable to find intreface index : %s\n", strerror(errno));
         exit(-1);
-    }*/
+    }
+    char *a = get_interface_name();
+    printf("%s", a);*/
 
-    strcpy(ifr.ifr_name, "eth0");
-    octl(sockfd, SIOCGIFINDEX, &ifr);
+    strcpy(ifr.ifr_name, "enp4s0f1");
+
+    if (ioctl(listen_socket, SIOCGIFINDEX, &ifr) == -1) {
+        fprintf(stderr ,"ioctl error,no such interface\n");
+        close(listen_socket);
+        exit(-1);
+    }
 
     struct sockaddr_ll sll;
     memset(&sll, 0, sizeof(struct sockaddr_ll));
     sll.sll_family = AF_PACKET;
     //sll.sll_protocol = htons(0x1122);
     sll.sll_protocol = htons(ETH_P_ALL);
-
     sll.sll_ifindex = ifr.ifr_ifindex;
-    //ssl.sll_hatype = ARPHRD_ETHER;  //
-    //ssl.sll_pkttype = PACKET_OTHERHOST;  //
-    //ssl.sll_sll_halen = sll_hatype; //
+    //ssl.sll_hatype = ARPHRD_ETHER;
+    //ssl.sll_pkttype = PACKET_OTHERHOST;
+    //ssl.sll_sll_halen = sll_hatype;
 
     if ( -1 == ( bind(listen_socket, (struct sockaddr *)&sll, sizeof(struct sockaddr_ll)) ) )
     {
         fprintf(stderr ,"bind error\n");
         exit(-1);
     }
-    //   then,
-    //   n = recvfrom(socketlisten, buffer, BUFFER_MAX, 0, NULL, NULL);
-    //   sendto(skfd, buf, pks_len, 0, (struct sockaddr*)target, sizeof(struct sockaddr_ll));
 
-    /**********************************/
-    struct event *base = event_base_new();
+/*
+    int a, i;
+    char buffer[BUFFER_MAX];
+    while(1)
+    {
+        printf("recvfrom\n");
+        a = recvfrom(listen_socket, buffer, BUFFER_MAX, 0, NULL, NULL);
+        for ( i=0 ; i < a ; i++)
+            printf("%.2X ",(unsigned char)buffer[i]);
+            if(((i+1)%16)==0) printf("\n");
+    }
+    //test
+*//*
+    char dest_addr[6];
+    dest_addr[0] = dest_addr[1] = dest_addr[2] = dest_addr[3] = dest_addr[4] = dest_addr[5] = 0xFF;
+    rsync_info.local_mac = malloc(sizeof( char) * 6);
+    rsync_info.local_mac[0] = rsync_info.local_mac[1] =rsync_info.local_mac[2] = rsync_info.local_mac[3] = rsync_info.local_mac[4] = rsync_info.local_mac[5] = 0xFF;
+    printf("start reply()\n");
+    p_reply(dest_addr, 3);
+    printf("end reply()\n");
+//    //test*/
+
+    struct event_base *base = event_base_new();
     struct event *listen_event;
-    listen_event = event_new(base, listen_socket, EV_HEAD|EV_PERSIST, p_read_callback, struct config);
+    listen_event = event_new(base, listen_socket, EV_READ|EV_PERSIST, p_read_callback, NULL);
+
     event_add(listen_event, NULL);
 
     pthread_t sync_tid;
     pthread_create(&sync_tid, NULL, sync_thread, NULL);
 
+    fprintf(stdout, "- START LISTENING -\n");
     event_base_dispatch(base);
 }
 
 
-void sync_thread(void *arg){
-    struct event *base = event_base_new();
+void *sync_thread(void *arg){
+    struct event_base *base = event_base_new();
     struct timeval timer={.tv_sec = 30, .tv_usec = 0};
     struct event sync_ev;
 
-    int send_socket = socket(PF_INET, SOCK_RAW, 0);     //send
+    int send_socket = socket(PF_INET, SOCK_RAW, htons(ETH_P_ALL));
     if ( send_socket < 0 )
     {
         fprintf(stderr ,"Failed to create socket\n");
         exit(-1);
     }
 
-    struct ifreq ifr;
-    char *ip = inet_ntoa(((struct sockaddr_in *)ifap->ifa_addr)->sin-addr);
-    if (ioctl(listen_socket, SIOCGIFINDEX, &ifr) == -1){
-        fprintf(stderr, "unable to find interface index : %s\n", strerror(errno));
-        exit(-1);
-    }
-
-    struct sockaddr_ll sll;
-    memset(&sll, 0, sizeof(struct sockaddr_ll));
-    sll.sll_family = PF_PACKET;
-    sll.sll_ifindex = ifr.ifr_ifindex;
-    sll.sll_protocol = htons(ETH_P_APLL);
-    if(bind(send_socket, &addr, sizeof(addr)) < 0)
-    {
-        perror("send bind error");
-        exit(1);
-    }
-
-    event_set(&sync_ev, send_socket, EV_PERSIST, p_sync_callback, struct rsync_info *rsync_info);
+    event_set(&sync_ev, send_socket, EV_PERSIST, p_sync_callback, &rsync_info);
     event_base_set(base, &sync_ev);
     event_add(&sync_ev, &timer);
+
     event_base_loop(base, 0);
+    return 0;
 }
 
 
-
+/*******************************/
 void set_mac(char *setto, char *from){
     int i;
     for( i = 0 ; i < 6; ++i )
@@ -153,121 +115,126 @@ void set_mac(char *setto, char *from){
     }
 }
 
-void read_thread(char buffer[]){
-    int i;
-    eth = (struct ethhdr*)buffer;
+void read_thread(void *arg){
+    struct read_callback_buffer *buffer_arg = (struct read_callback_buffer *)arg;
 
-    char *dmac = (char *)malloc(len(char) * 6);
-    char *smac = (char *)malloc(len(char) * 6);
+    int i;
+    char *dmac = (char *)malloc(sizeof(char) * 7);
+    char *smac = (char *)malloc(sizeof(char) * 7);
     for ( i = 0 ; i < 6; ++i )
     {
-        dmac[i] = (unsigned char)buffer[i];
-        smac[i] = (unsigned char)buffer[i+6];
+        dmac[i] = (unsigned char)buffer_arg->buffer[i];
+        smac[i] = (unsigned char)buffer_arg->buffer[i+6];
     }
     dmac[++i] = '\0'; smac[++i] ='\0';
 
-    char *eth_type = ((unsigned char)buffer[13])*16*16 + (unsigned char)buffer[14];
-    if (eth_type == 0x1122 && is_corrent_mac(smac, dmac) ){
+    uint16_t eth_type = ((uint16_t)buffer_arg->buffer[13]) << 8 | (uint16_t)buffer_arg->buffer[14];
+    if (eth_type == 0x1122 /*&& is_corrent_mac(smac, dmac) */){
+
         //to hyr
     }
+    printf("-TO HYR TO HYR!-");
+
+
 }
-
-
 
 int is_corrent_mac(char *dmac, char *smac)
 {
     char mac[7];
     int i;
     int flag = 0;
-    if( !( strcmp(rsync_info->conifg_mac, dmac) != 0 && strcmp(rsync_info->local_mac, dmac) != 0 ) )
+    if( !( strcmp(rsync_info.conifg_mac, dmac) != 0 && strcmp(rsync_info.local_mac, dmac) != 0 ) )
         return 0;
-    for( i = 0 ; i < rsync_info->mac_num ; ++i )
+    for( i = 0 ; i < rsync_info.mac_num ; ++i )
     {
-        strncpy(mac, rsync_info->listen_mac + 6 * i, 6);
+        strncpy(mac, rsync_info.listen_mac + 6 * i, 6);
         if (strcmp(smac, mac) == 0)
         {
             flag = 1;
             break;
         }
     }
-    (flag == 1)? return 1: return 0;
+    if (flag == 1)
+        return 1;
+    else
+        return 0;
 }
-
 
 /******************************/
-int p_read_callback(int sock, short event, void *arg){
+
+void p_read_callback(int sock, short event, void *arg){
+
+    printf("-RECIVED PACKET-\n");
+
     char buffer[BUFFER_MAX];
-    struct ethhdr *eth;
+    int len;
 
-    n_read = recvfrom(sock, buffer, BUFFER_MAX, 0, NULL, NULL);
+    len = recvfrom(sock, buffer, BUFFER_MAX, 0, NULL, NULL);
 
-    /*
-    if (n_read < 32){
-    }
-    printf("-START-\n");
-    char *eth_type = ((unsigned char)buffer[16])*16*16 + (unsigned char)buffer[17];
-    if (eth_type == 0x1122){
-        for ( i=0 ; i<n_read ; i++)
+    printf("length : %d\n", len);
+    if(len < 23)
+        return;
+
+    uint16_t eth_type = ((uint16_t)buffer[12]) << 8 | (uint16_t)buffer[13];
+
+
+    if (eth_type == 0x0800){
+        int i;
+        for ( i=0 ; i<len ; i++){
             printf("%.2X ",(unsigned char)buffer[i]);
             if(((i+1)%16)==0) printf("\n");
+        }
     }
-    printf("\n-END-\n");
-    return 0;*/
+
+
+
+
+    struct read_callback_buffer *buffer_arg = malloc(sizeof(struct read_callback_buffer));
+    strncpy(buffer_arg->buffer, buffer, len);
+    buffer_arg->len = len;
 
     pthread_t sync_tid;
-    pthread_create(&read_thread, NULL, read_thread, buffer);
+    pthread_create(&sync_tid, NULL, (void * (*)(void *))&read_thread, buffer_arg);
+
+    printf("\n-END PACKET-\n");
+
 }
 
 
-int p_sync_callback(int send_socket, short event, struct rsync_info *rsync_info){
-    struct rsync_info *rsync_info = arg;
-    int mac_num = rsync_info->rsync_info;
-    char *mac = rsync_info->mac;
+void p_sync_callback(int send_socket, short event, void *arg){
+    fprintf(stdout, "- A SYNC START -\n");
+    struct rsync_info *_rsync_info = arg;
+
+/*
+    int mac_num = _rsync_info->mac_num;
+    char *mac = _rsync_info->listen_mac;
     char dest_addr[6];
+    int len = _rsync_info->len;
 
     for ( ; mac_num > 0 ; --mac_num )
     {
         strncpy(dest_addr, mac, 6);
-        csismp_send(send_socket, dest_addr, type, rsync_info->tlvs);
+        csismp_send(send_socket, dest_addr, 5, &_rsync_info->tlvs, len);
         mac += 6;
     }
+*///!
+
+    fprintf(stdout, "- A SYNC END -\n");
 }
 
-
-/************************************/
-int p_reply(char dest_addr[6], int type){
-
-    int send_socket = socket(PF_INET, SOCK_RAW, 0);     //send
-
-    struct ifreq ifr;
-
-    strcpy(ifr.ifr_name, "eth0");
-    octl(sockfd, SIOCGIFINDEX, &ifr);
-
-
-    struct sockaddr_ll sll;
-    memset(&sll, 0, sizeof(struct sockaddr_ll));
-    sll.sll_family = PF_PACKET;
-    sll.sll_ifindex = ifr.ifr_ifindex;
-    sll.sll_protocol = htons(ETH_P_APLL);
-    if(bind(send_socket, &addr, sizeof(addr)) < 0)
+void p_reply(char dest_addr[6], int type){
+    int reply_socket = socket(PF_INET, SOCK_RAW, htons(ETH_P_ALL));
+    if ( reply_socket < 0 )
     {
-        perror("send bind error");
-        exit(1);
+        fprintf(stderr ,"Failed to create socket\n");
+        exit(-1);
     }
 
-
-    csismp_send(send_socket, dest_addr, type, NULL);
-
-    close(send_socket);
-    //close send_socket
+    csismp_send(reply_socket, dest_addr, type, NULL, 0);
+    close(reply_socket);
 }
 
-
-/************************************/
-//void ntoh(unsigned char *dbuf, size_t len){
-//
-//}
+/******************************************/
 
 char* csismp_construct(
         char source_addr[6],
@@ -277,36 +244,73 @@ char* csismp_construct(
         int end,
         int slice,
         int session,
-        const char* s_tlvs)
+        const char* s_tlvs,
+        int len)
 {
-    struct packet *csismp;
+    struct packet *csismp = malloc(sizeof(struct packet));
 
-    set_mac(csismp.smac, dest_addr);
-    set_mac(csismp.dmac, //sendmac);
+    set_mac(csismp->smac, source_addr);
+    set_mac(csismp->dmac, dest_addr);
 
-    csismp.pro_type = htons(0x1122);
-    csismp.c_type = type;
+    csismp->pro_type = htons(0x1122);
+    csismp->c_type = c_type;
 
-    csismp.start = start;
-    csismp.end = end;
-    csismp.slice = slice;
-    csismp.session = session;
-    strcpy(cssismp.tlvs, s_tlvs);
+    csismp->start = start;
+    csismp->end = end;
+    csismp->slice = slice;
+    csismp->session = session;
+    strncpy(csismp->tlvs, s_tlvs, len);
 
-    return cssismp;
+    return (char*)csismp;
 }
 
+int _csismp_send(int send_socket, const char *buffer, int len){
+    int raw_send;
 
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy (ifr.ifr_name, "enp4s0f1", sizeof(ifr.ifr_name) - 1);
+    ifr.ifr_name[sizeof(ifr.ifr_name)-1] = '\0';
 
+    if (ioctl(send_socket, SIOCGIFINDEX, &ifr) == -1) {
+        fprintf(stderr ,"ioctl error,no such interface\n");
+        close(send_socket);
+        exit(-1);
+    }
 
-int _csismp_send(int send_socket, const char *buffer){
-    sendto(send_socket, buffer, len(buffer), 0, &addr, addr_len);//!
+    ioctl(send_socket, SIOCGIFFLAGS, &ifr);
+    if ( (ifr.ifr_flags & 0x1) == 0) {
+        fprintf(stderr ,"ioctl error,the interface is down\n");
+        close(send_socket);
+    }
+    ioctl(send_socket, SIOCGIFINDEX, &ifr);
+
+    struct sockaddr_ll sll;
+    memset(&sll, 0, sizeof(struct sockaddr_ll));
+    //sll.sll_family = AF_PACKET;
+    //sll.sll_protocol = htons(ETH_P_ALL);
+    sll.sll_ifindex = ifr.ifr_ifindex;
+//
+    fprintf(stdout, "sned lens:%d\n", len);
+    int i;
+    for ( i=0 ; i<len ; i++){
+        fprintf(stdout, "%.2X ",(unsigned char)buffer[i]);
+        if(((i+1)%16)==0) printf("\n");
+    }
+    fprintf(stdout,"print end\n");
+//
+    if (raw_send = sendto(send_socket, buffer, len, 0,
+                    (struct sockaddr *) &sll ,sizeof(struct sockaddr_ll)) ==-1)
+    {
+        fprintf(stderr ,"_csismp_send error\n");
+        return -1;
+    }
+    return 0;
 }
 
-int csismp_send(int send_socket, char dest_addr[6], int type, int tlvs){
-    char *s_tlvs = tlvs; //!
-    int s_len = len(s_tlvs);
-    char *buffer;
+int csismp_send(int send_socket, char dest_addr[6], int type, char* tlvs, int s_len){
+    char *s_tlvs = tlvs;
+    char *buffer = malloc(sizeof(char) * BUFFER_MAX);
     int end;
     int slice; // slice
     static int session = 0; // session id
@@ -316,9 +320,9 @@ int csismp_send(int send_socket, char dest_addr[6], int type, int tlvs){
     if (type == 3 || type == 4)
     {
         //rand int <1000 return randint1000
-        buffer = csismp_construct(sourc_addr, dest_addr,
-                            type, 1, 1, 1, session, NULL);
-        _csismp_send(send_socket, buffer);
+        buffer = csismp_construct(rsync_info.local_mac, dest_addr,
+                            type, 1, 1, 1, session, "\0",1);
+        _csismp_send(send_socket, buffer, 23);
     }
     else if (type == 5)
     {
@@ -328,10 +332,40 @@ int csismp_send(int send_socket, char dest_addr[6], int type, int tlvs){
 
             srand((unsigned)time(NULL));
             //rand int > 1000 return randint
-            buffer = csismp_construct(source_addr, dest_addr,
-                            type, slice? 0:1, (s_len < 1024)? 1:0, slice, session, s_tlvs);
-            _csismp_send(send_socket, buffer);
+            buffer = csismp_construct(rsync_info.local_mac, dest_addr,
+                            type, slice? 0:1, (s_len < 1024)? 1:0, slice, session + 1000, s_tlvs, s_len % 1024);
+            _csismp_send(send_socket, buffer, 22 + s_len % 1024);
             s_tlvs -= 1024;
         }
     }
 }
+
+
+char *get_interface_name(){
+    struct ifaddrs *ifa = NULL, *ifList;
+    char *interface_name = malloc(sizeof(char) * 12);
+    interface_name[0] = '\0';
+
+    if (getifaddrs(&ifList) < 0)
+    {
+        return NULL;
+    }
+
+
+    for (ifa = ifList; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if(ifa->ifa_addr->sa_family == AF_INET)
+        {
+            if (strlen(interface_name) == 0)
+                strcpy(interface_name, ifa->ifa_name);
+            else
+                if (strcmp(interface_name, ifa->ifa_name) > 0)
+                    strcpy(interface_name, ifa->ifa_name);
+        }
+    }
+
+    freeifaddrs(ifList);
+    return interface_name;
+}
+
+
--- async/async.h
+++ async/async.h
@@ -3,20 +3,29 @@
 
 
 
-#include <string.h>
-#include <stdlib.h>
+#include <net/if.h>
+#include <netpacket/packet.h>
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#include <fcntl.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <sys/types.h>
+#include <linux/if_ether.h>
+#include <ifaddrs.h>
+//#include <linux/in.h>
 
 #include <event.h>
+#include <event2/event.h>
 #include <pthread.h>
-#include <pcap.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+
+#define BUFFER_MAX 1049
+
+
 
 struct rsync_info{
     char *conifg_mac;
@@ -24,9 +33,38 @@ struct rsync_info{
     int mac_num;
     char *listen_mac;
     char tlvs;
+    int len;
 }rsync_info;
 
 
+typedef struct packet
+{
+    char smac[6];
+    char dmac[6];
+    short pro_type;
+    struct
+    {
+        char c_type;
+        int start:1;
+        int end:1;
+        int slice:22;
+        int session;
+    };
+    char tlvs[BUFFER_MAX];
+} packet;
+
+struct read_callback_buffer{
+    char buffer[BUFFER_MAX];
+    int len;
+};
+
+
+void p_read_callback(int sock, short event, void *arg);
+void p_sync_callback(int send_socket, short event, void *arg);
+void p_reply(char dest_addr[6], int type);
+void *sync_thread(void *arg);
+int csismp_send(int send_socket, char dest_addr[6], int type, char* tlvs, int s_len);
+char *get_interface_name();
 
 
 #endif
